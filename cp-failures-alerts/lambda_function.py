018fdfa60f08ebca336e75401f8c9e35a3917b9b
EC2 Default User <ec2-user@ip-172-31-38-202.ec2.internal>
2020-02-16 05:21:27 +0000

    adding logic related to cp alerting

---
+++ cp-failures-alerts/lambda_function.py
@@ -0,0 +1,34 @@
+import json
+import os
+import logging
+import boto3
+import sys
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+cp_client=boto3.client('codepipeline')
+sns_client = boto3.client('sns')
+sns_topic_arn=os.getenv(sns_topic_arn)
+
+def cp-failures-alerts(event, context):
+    # TODO implement
+    print (event)
+    pipelineName=event['detail']['pipeline']
+    executionID=event['detail']['execution-id']
+    state=event['detail']['state']
+    stage=event['detail']['stage']
+    response=cp_client.get_pipeline_state(name=pipelineName)
+    print (response)
+    print ("Printing stage level issues")
+    for i in response['stageStates']:
+        if i['actionStates'][0].get('latestExecution') is not None:
+            errorMessage=i['actionStates'][0]['latestExecution']['errorDetails']['message']
+            errorCode=i['actionStates'][0]['latestExecution']['errorDetails']['code']
+            message="Stage: "+stage+" failed due to "+errorMessage+" with errorCode "+errorCode
+    send_notification(message)
+    
+def send_notification(message):
+    response = sns_client.publish(
+    TopicArn=sns_topic_arn,   
+    Message=message
+    )
