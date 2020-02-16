58c5681b4f88adedf2e37e515ce574c99bb855f0
EC2 Default User <ec2-user@ip-172-31-38-202.ec2.internal>
2020-02-16 05:24:48 +0000

    added comments to the lambda function

--- cp-failures-alerts/lambda_function.py
+++ cp-failures-alerts/lambda_function.py
@@ -11,20 +11,22 @@ sns_client = boto3.client('sns')
 sns_topic_arn=os.getenv(sns_topic_arn)
 
 def cp-failures-alerts(event, context):
-    # TODO implement
-    print (event)
+    #Setting params
     pipelineName=event['detail']['pipeline']
     executionID=event['detail']['execution-id']
     state=event['detail']['state']
     stage=event['detail']['stage']
+    
+    #Getting  pipeline status on basis of the above executionID
     response=cp_client.get_pipeline_state(name=pipelineName)
-    print (response)
+    
     print ("Printing stage level issues")
     for i in response['stageStates']:
         if i['actionStates'][0].get('latestExecution') is not None:
             errorMessage=i['actionStates'][0]['latestExecution']['errorDetails']['message']
             errorCode=i['actionStates'][0]['latestExecution']['errorDetails']['code']
             message="Stage: "+stage+" failed due to "+errorMessage+" with errorCode "+errorCode
+    #Sending alerts
     send_notification(message)
     
 def send_notification(message):
